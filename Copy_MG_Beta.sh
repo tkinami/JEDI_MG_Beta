54a80934ab57084e1deddad488539d4ea2166f14
Teppei Kinami <teppei.kinami@noaa.gov>
2020-10-09 02:19:54 -0500

    modify PATH

--- Copy_MG_Beta.sh
+++ Copy_MG_Beta.sh
@@ -6,7 +6,7 @@ cp fv3-bundle/fv3-jedi/test/CMakeLists.txt $WORK_JEDI/src/fv3-bundle/fv3-jedi/te
 for FILE in convertstate_gfs_c96 dirac_gfs_bal_rf_c96 dirac_gfs_bal_mgbf_c96 
 do 
   cp fv3-bundle/fv3-jedi/test/testinput/${FILE}.yaml $WORK_JEDI/src/fv3-bundle/fv3-jedi/test/testinput/${FILE}.yaml
-  cp fv3-bundle/fv3-jedi/test/testoutput/${FILE}.ref $WORK_JEDI/src/fv3-bundle/fv3-jedi/test/testout/${FILE}.ref
+  cp fv3-bundle/fv3-jedi/test/testoutput/${FILE}.ref $WORK_JEDI/src/fv3-bundle/fv3-jedi/test/testoutput/${FILE}.ref
 done
 
 cp fv3-bundle/saber/src/saber/CMakeLists.txt $WORK_JEDI/src/fv3-bundle/saber/src/saber/CMakeLists.txt
